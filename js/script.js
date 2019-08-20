40533f83e5c025e753874ab22ef7004461430757
Liberty <skyhawkvizor@gmail.com>
2019-08-20 23:40:34 +0200

    1
    1

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let hoverItem = $('.collection-content__item');
-hoverItem.on('mouseenter', function() {
-    $(this).find("img").addClass('brightDown');
-});
-hoverItem.on('mouseleave', function() {
-    $(this).find("img").removeClass('brightDown');
-});
