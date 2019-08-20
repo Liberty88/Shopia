6cf56addf3e1f7ef4832e2894cec9a78e7fac289
Liberty <skyhawkvizor@gmail.com>
2019-08-20 23:41:30 +0200

    1
    1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let hoverItem = $('.collection-content__item');
+hoverItem.on('mouseenter', function() {
+    $(this).find("img").addClass('brightDown');
+    $(this).find("button").addClass('color-change');
+});
+hoverItem.on('mouseleave', function() {
+    $(this).find("img").removeClass('brightDown');
+    $(this).find("button").removeClass('color-change');
+
+});
+
+$('.banner-section').slick({
+    fade: true,
+    dots: true,
+    arrows: false,
+    // autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                dots: false,
+            }
+        }
+    ]
+});
